7a005b35bf26eb99815898ae37c94f52c1649d8a
CiprianChiches13 <cipipowerlifting22@gmail.com>
2023-05-10 12:45:32 +0300

    first commit

---
+++ src/components/Attribution.js
@@ -0,0 +1,9 @@
+function Attribution() {
+   return (
+      <footer className="attribution">
+         <p>developed by <a href="https://ciprianchiches.netlify.app/" target="_blank" rel="noreferrer">Ciprian Chiches</a></p>
+      </footer>
+   );
+}
+
+export default Attribution;
